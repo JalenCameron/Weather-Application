3ec2dc1a71564c261f5d031b4232868905735c63
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-10-10 23:58:54 -0700

    Loading an icon to associate with the current weather

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Weather = ({weather}) => {
+    console.log(weather)
   return (
-    <div>Weather</div>
+    <div>
+        <div>
+            <div>
+                <Image src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt='The current weather' width="100" height="100" />
+            </div>
+        </div>
+    </div>
   )
 }
 
