7660e5ea4c9a03d59f6963b2c647a3d240982af5
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-10-10 21:56:35 -0700

    Weather data fetched upon search request

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ import { useState } from 'react';
 import { BsSearch } from 'react-icons/bs'
 
 export default function Home() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=vancouver&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
-
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
   const fetchWeather = (e) => {
     e.preventDefault()
@@ -44,7 +44,23 @@ export default function Home() {
       />
 
       {/* Input for Search */}
-
+      <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10 '>
+        <form onSubmit={fetchWeather} className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl'>
+          <div>
+            <input 
+              onChange={(e) => setCity(e.target.value)}
+              className='bg-transparent border-none text-white focus:outline-none text-2xl' 
+              type="text" 
+              placeholder='Search for a City' 
+            />
+          </div>
+          <button onClick={fetchWeather}>
+            <BsSearch size={20} />
+          </button>
+        </form>
+      </div>
+      {/* Displaying the Weather */}
+      
     </div>
   );
 }
