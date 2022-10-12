1325aad78bf32162b89008ff7a6d7c86af43f253
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-10-11 23:09:43 -0700

    Some simple styling adjustments

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -4,8 +4,9 @@ import { data } from 'autoprefixer'
 
 const Weather = ({weather}) => {
     console.log(weather)
+    const description = weather.weather[0].description;
   return (
-    <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10'>
+    <div className='relative flex flex-col justify-evenly max-w-[600px] w-full h-[100vh] m-auto p-4 text-gray-300 z-10'>
         {/* Top */}
         <div className='relative flex justify-between pt-12'>
             <div className='flex flex-col items-center'>
@@ -16,7 +17,7 @@ const Weather = ({weather}) => {
         </div>
         {/* Bottom */}
         <div className='bg-black/50 relative p-8 rounded-md'>
-            <p className='text-2xl text-center pb-6'>Weather in {weather.name}</p>
+            <p className='text-2xl text-center pb-6'>Weather in {weather.name}, {weather.sys.country}</p>
             <div className='flex justify-between text-center'>
                 <div>
                     <p className='font-bold text-2xl'>{weather.main.feels_like.toFixed(0)}&#176;</p>
@@ -31,6 +32,11 @@ const Weather = ({weather}) => {
                 <div>
                     <p className='font-bold text-2xl'>{weather.wind.speed.toFixed(0)} MPH</p>
                     <p className='text-xl'>Winds</p>
+                </div>
+
+                <div>
+                    <p className='font-bold text-2xl'>{description.charAt(0).toUpperCase() + description.slice(1)}</p>
+                    <p className='text-xl'>Description</p>
                 </div>
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
 
         {/* Background Image pulled from Unsplash */}
         <Image
-          src="https://images.unsplash.com/photo-1562806568-a3450b810449?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+          src="https://images.unsplash.com/photo-1580193483760-d0ef2abaa348?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
           alt="Sunny sky with clouds"
           layout="fill"
           className="object-cover"
