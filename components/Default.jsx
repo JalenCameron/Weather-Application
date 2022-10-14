2d014df9a61c65c8110b1a46971feb65991eadd0
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-10-14 13:12:46 -0700

    Sunset and Sunrise set to local time

--- components/Default.jsx
+++ components/Default.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 const Default = () => {
   return (
     <Image
-        src="https://images.unsplash.com/photo-1580193483760-d0ef2abaa348?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
-        alt="A Sunny mountainview with clouds"
+        src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80"
+        alt="A View of the City from Way Up High"
         layout="fill"
         className="object-cover"
     />
--- components/Weather.jsx
+++ components/Weather.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
+import moment from 'moment';
 import { data } from 'autoprefixer'
 
 const Weather = ({weather}) => {
     console.log(weather)
+    // A function to fetch the Weather description
     const description = weather.weather[0].description;
+    // A function to fetch the sunrise and sunset
+    const sunrise = moment.unix(weather.sys.sunrise).format("LT")
+    const sunset = moment.unix(weather.sys.sunset).format("LT")
 
   return (
-    <div className='relative flex flex-col justify-evenly max-w-[600px] w-full h-[100vh] m-auto p-4 text-gray-300 z-10'>
+    <div className='relative flex flex-col justify-evenly max-w-[750px] w-full h-[100vh] m-auto p-4 text-gray-300 z-10'>
 
         {/* Weather Temperature */}
         <div className='relative flex justify-between pt-12'>
@@ -23,23 +28,23 @@ const Weather = ({weather}) => {
             <p className='text-2xl text-center pb-6'>Weather in {weather.name}, {weather.sys.country}</p>
             <div className='flex justify-between text-center'>
                 <div>
-                    <p className='font-bold text-2xl'>{weather.main.feels_like.toFixed(0)}&#176;</p>
                     <p className='text-xl'>Feels Like</p>
+                    <p className='font-bold text-2xl'>{weather.main.feels_like.toFixed(0)}&#176;</p>
                 </div>
 
                 <div>
-                    <p className='font-bold text-2xl'>{weather.main.humidity}%</p>
-                    <p className='text-xl'>Humidity</p>
+                    <p className='text-xl'>Sunrise</p>
+                    <p className='font-bold text-2xl'>{sunrise}</p>
                 </div>
 
                 <div>
-                    <p className='font-bold text-2xl'>{weather.wind.speed.toFixed(0)} MPH</p>
-                    <p className='text-xl'>Winds</p>
+                    <p className='text-xl'>Sunset</p>
+                    <p className='font-bold text-2xl'>{sunset}</p>
                 </div>
 
                 <div>
-                    <p className='font-bold text-2xl' style={{textTransform: 'capitalize'}}>{description}</p>
                     <p className='text-xl'>Description</p>
+                    <p className='font-bold text-2xl' style={{textTransform: 'capitalize'}}>{description}</p>
                 </div>
             </div>
         </div>
