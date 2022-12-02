43bb6048f2a909aa1e619ad997c39b8f991d00a5
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-12-01 16:30:27 -0800

    Final modal touch up

--- components/Modal.js
+++ components/Modal.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 const Modal = ({ show, onClose, children, title }) => {
+  /* Adding an isBrowser boolean and setting it to true in the initial render of the component. This way, on the initial load of the page the rendering on the server will not give me an 'undefined' error when I set up the modal */
   const [isBrowser, setIsBrowser] = useState(false);
 
   useEffect(() => {
@@ -30,6 +31,7 @@ const Modal = ({ show, onClose, children, title }) => {
   ) : null;
 
   if (isBrowser) {
+    // Conditionally creating the portal depending on whether the code executes in the browser or not.
     return ReactDOM.createPortal(
       modalContent,
       document.getElementById("modal-root")
