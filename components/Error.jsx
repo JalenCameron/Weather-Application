970acf8aa7acf2bc91f8a0a920df7486d8d0ccdc
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-12-02 20:44:08 -0800

    Fixed error handling page

--- components/Error.jsx
+++ components/Error.jsx
@@ -10,10 +10,10 @@ function Error() {
     <div className="errorOverlay text-center bg-[#101010] absolute top-0 left-0 right-0 bottom-0">
         <div className='errorWrapper'>
             <div className='error top-[15rem] absolute bottom-0 left-0 right-0'>
-                <p className='text-[snow]'>
+                <p className='text-[snow] max-w-[300px] mx-auto sm:max-w-md'>
                 Unfortunately you may have entered the wrong name or incorrectly spelled the city you are looking for. Please return home and try again.
                 </p>
-                <div className='text-[snow] my-8 font-bold underline'>
+                <div className='text-[snow] my-8 font-bold underline max-w-[300px] mx-auto sm:max-w-md'>
                     <button onClick={refresh}>Go Home!</button>
                 </div>
             </div>
